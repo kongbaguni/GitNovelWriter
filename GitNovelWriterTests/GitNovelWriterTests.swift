fd7c0b279076b04a20671e063dbfc07bc22f3e66
서창열 <scy@jkds.kr>
2026-07-15 09:10:15 +0900

    Initial Commit

---
+++ GitNovelWriterTests/GitNovelWriterTests.swift
@@ -0,0 +1,18 @@
+//
+//  GitNovelWriterTests.swift
+//  GitNovelWriterTests
+//
+//  Created by 서창열 on 7/15/26.
+//
+
+import Testing
+
+struct GitNovelWriterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
